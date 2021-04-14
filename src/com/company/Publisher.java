411509720fc1ad8bf3e5baabe5ea0bfda283aa13
Heba Alnouri <hebaalnouri1029@gmail.com>
2021-04-14 21:22:29 +0300

    Implemented Publisher Class.

--- src/com/company/Publisher.java
+++ src/com/company/Publisher.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Publisher {
+    int publisherId;
+    String publisherName;
+    String phoneNumber;
+    String email;
+
+    public Publisher(int publisherId, String publisherName, String phoneNumber, String email) {
+        this.publisherId = publisherId;
+        this.publisherName = publisherName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
