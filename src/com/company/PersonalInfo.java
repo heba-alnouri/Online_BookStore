237de2a6b41cd0d5c5dc99e93f6e448526bdc827
Heba Alnouri <hebaalnouri1029@gmail.com>
2021-04-14 21:20:28 +0300

    Implemented PersonalInfo Class.

--- src/com/company/PersonalInfo.java
+++ src/com/company/PersonalInfo.java
@@ -1,4 +1,59 @@
 package com.company;
 
+import java.util.Date;
+
 public class PersonalInfo {
+    int userId;
+    String userFirstName;
+    String userLastName;
+    String userEmail;
+    Date userDateOfBirth;
+
+    public PersonalInfo(int userId, String userFirstName, String userLastName, String userEmail, Date userDateOfBirth) {
+        this.userId = userId;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+        this.userDateOfBirth = userDateOfBirth;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Date getUserDateOfBirth() {
+        return userDateOfBirth;
+    }
+
+    public void setUserDateOfBirth(Date userDateOfBirth) {
+        this.userDateOfBirth = userDateOfBirth;
+    }
 }
